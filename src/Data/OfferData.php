5406abbd02fda2699bcc6a9846c319634a281738
Evgeny Semenov <geekco@yandex.ru>
2026-06-10 17:04:10 +0300

    fix: offers

--- src/Data/OfferData.php
+++ src/Data/OfferData.php
@@ -8,7 +8,6 @@ use Carbon\Carbon;
 use Spatie\LaravelData\Attributes\DataCollectionOf;
 use Spatie\LaravelData\Attributes\Validation\ArrayType;
 use Spatie\LaravelData\Attributes\Validation\Distinct;
-use Spatie\LaravelData\Attributes\Validation\Min;
 use Spatie\LaravelData\Attributes\Validation\Nullable;
 use Spatie\LaravelData\Attributes\Validation\Required;
 use Spatie\LaravelData\Attributes\Validation\StringType;
@@ -20,10 +19,10 @@ class OfferData extends Data
     public function __construct(
         #[Required, StringType, Uuid]
         public string $product_id,
-        #[Required, ArrayType, Min(1), DataCollectionOf(OfferPriceData::class)]
-        public array $prices,
         #[Nullable, StringType, Uuid]
         public ?string $variant_id = null,
+        #[Nullable, ArrayType, DataCollectionOf(OfferPriceData::class)]
+        public ?array $prices = null,
         #[Nullable, ArrayType, Distinct, DataCollectionOf(StockData::class)]
         public ?array $stocks = null,
         #[Nullable]
--- src/Repositories/OfferRepository.php
+++ src/Repositories/OfferRepository.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 namespace GeekCo\CommerceJson\Repositories;
 
 use GeekCo\CommerceJson\Models\Offer;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
 
 class OfferRepository extends BaseRepository
 {
     public function __construct(Offer $model)
     {
         parent::__construct($model);
+    }
+
+    public function paginate(int $perPage = 15): LengthAwarePaginator
+    {
+        return $this->model->with('prices', 'stocks')->paginate($perPage);
     }
 
     public function findByProduct(string $productId): array
